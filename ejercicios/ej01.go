5d79284ea060275861a8a773ef26f9ea65bf7754
Diamela Riccio <diamelariccio24@hotmail.com>
2025-06-06 00:19:33 -0300

    ejercicios

--- ejercicios/ej01.go
+++ ejercicios/ej01.go
@@ -19,9 +19,24 @@ func SecondLargestElement(bt *binarytree.BinarySearchTree[int]) (int, error) {
 func findSecLargeElem(n *binarytree.BinaryNode[int]) (int, error) {
 
 	var x int
+	x = n.GetLeft().GetData()
+
+	if n == nil {
+		return 0, errors.New("no hay valores")
+	}
+
+	if n.GetRight() == nil {
+		n = n.GetLeft()
+		for n.GetRight() != nil {
+			n = n.GetRight()
+			x = n.GetData()
+		}
+	}
+
 	for n.GetRight() != nil {
 		x = n.GetData()
 		n = n.GetRight()
 	}
+
 	return x, nil
 }
